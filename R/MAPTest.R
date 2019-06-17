a08eba8980f04c6c3ac00acd8d93b0472cb749b3
Meng Cao <meca7653@gmail.com>
2019-06-17 13:48:54 -0600

    small typo

--- R/MAPTest.R
+++ R/MAPTest.R
@@ -1,8 +1,8 @@
 #' @details
 #' This package contains functions for statistical calculations and multiple testing procedures.
 #' @author Meng Cao <meng.cao@colostate.edu>, \cr
-#' F.Jay Breidt <FJay.Breidt@colostate.edu >, \cr
-#' Wen Zhou <riczw@stat.colostate.edu >
+#' F.Jay Breidt <FJay.Breidt@colostate.edu>, \cr
+#' Wen Zhou <riczw@stat.colostate.edu>
 #' @keywords internal
 #'
 "_PACKAGE"
